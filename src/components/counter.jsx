f4db3cf310f13847f2fd48da3342cad7b96fc1ef
Leired7 <leirediez.7@gmail.com>
2020-04-02 18:19:04 +0200

    y First React Component with Mosh Hamedani (part II)

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,12 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   render() {
-    return <h1>HELLO</h1>;
+    return (
+      <React.Fragment>
+        <h1>HELLO</h1>
+        <button>Increment</button>
+      </React.Fragment>
+    );
   }
 }
 
