f7a7346b79ff6d520234a8cbb8c5b2b441ef43e3
Leired7 <leirediez.7@gmail.com>
2020-04-04 17:11:19 +0200

    Removing the Local State (delete state property)

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,10 +1,6 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.counter.value,
-  };
-
   handleIncrement = (product) => {
     this.setState({ value: this.state.value + 1 });
   };
